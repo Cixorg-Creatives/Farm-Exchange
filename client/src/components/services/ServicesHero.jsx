b8fd7ec5e24757b86fab69f49c42eb72722354a1
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-10 13:51:48 +0530

    add ServicesHero component and update layout for Developers section in Services page

---
+++ client/src/components/services/ServicesHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesHero = () => {
+  return (
+    <div className='py-6 md:py-10 xl:py-14'>ServicesHero</div>
+  )
+}
+
+export default ServicesHero
--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,9 +1,11 @@
 import Developers from '@/components/services/Developers'
+import ServicesHero from '@/components/services/ServicesHero'
 import React from 'react'
 
 const Services = () => {
   return (
     <div className='px-4 md:px-6 lg:px-24'>
+      <ServicesHero />
       <Developers />
     </div>
   )
