45a36abd758cb92d9d4128f532ebafb44152026b
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-13 00:04:59 +0530

    refactor ServicesHero component for improved text sizing and layout consistency

--- client/src/components/services/ServicesHero.jsx
+++ client/src/components/services/ServicesHero.jsx
@@ -13,43 +13,44 @@ const ServicesHero = () => {
           </div>
           <div className="flex flex-col items-start justify-between gap-5 md:gap-7">
             <div className="flex flex-col gap-3 md:gap-4 lg:w-[43.75rem]">
-              <p className="capitalize text-[#747474] font-normal text-lg md:text-xl">
+              <p className="capitalize text-[#747474] font-normal text-base md:text-xl">
                 This is a premium chance to gain the access of premium The Forest Farms at right destination. The property offers customizable cottages with serene landscape gardens.
               </p>
-              <p className="capitalize text-[#1E1E1E] font-normal text-lg md:text-xl">
+              <p className="capitalize text-[#1E1E1E] font-normal text-base md:text-xl">
                 Nandi Hills, Chikballapur, Karnataka
               </p>
             </div>
-            <div className="flex gap-2 md:gap-4 flex-wrap md:flex-nowrap">
-              <p className="flex text-[#1E1E1E] font-bold text-4xl md:text-5xl text-end">
-                <IndianRupee className="text-[#1E1E1E] size-8 md:size-10" /> 1.75
-                <span className="text-3xl md:text-4xl pl-1 md:pl-2 h-full flex items-end">cr</span>
-              </p>
-              <p className="text-[#757575] font-normal text-lg md:text-xl h-full flex items-end justify-end pt-2.5 pb-0 md:pt-0 md:pb-0.5">
-                Onwards
-              </p>
+            <div className="flex gap-2 md:gap-4 h-full items-end">
+              <p className="flex items-end text-[#1E1E1E] font-bold text-4xl md:text-5xl">
+                <IndianRupee className="text-[#1E1E1E] size-8 md:size-10 -translate-y-2" />
+                1.75
+                <span className="text-2xl md:text-4xl pl-1 md:pl-2">cr</span>
+              </p> 
+              <p className="text-[#757575] font-normal text-base md:text-xl -translate-y-0.5">Onwards</p>
             </div>
           </div>
         </div>
         <div className="flex flex-col gap-3 md:gap-4 w-full lg:w-[30.5rem] mt-6 lg:mt-0">
-          <div className="bg-[#D9E1C3] p-4 md:p-5 w-full">
-            <div className="flex items-end justify-start gap-1 capitalize">
-              <p className="text-[#31511E] font-bold text-3xl md:text-4xl">10,890.0</p>
-              <p className="text-[#859F3E] font-bold text-xl md:text-2xl">Sq ft</p>
+          <div className='flex flex-row lg:flex-col gap-3 md:gap-4 w-full lg:w-[30.5rem] mt-6 lg:mt-0'>
+            <div className="bg-[#D9E1C3] p-3 md:p-5 w-full">
+              <div className="flex items-end justify-start gap-1 capitalize">
+                <p className="text-[#31511E] font-bold text-2xl md:text-4xl">10,890.0</p>
+                <p className="text-[#859F3E] font-bold text-lg md:text-2xl">Sq ft</p>
+              </div>
+              <p className="capitalize text-[#5A744B] text-base md:text-xl font-normal">Plot Area</p>
             </div>
-            <p className="capitalize text-[#5A744B] text-lg md:text-xl font-normal">Plot Area</p>
-          </div>
-          <div className="bg-[#D9E1C3] p-4 md:p-5 w-full">
-            <p className="text-[#31511E] font-bold text-3xl md:text-4xl">1607</p>
-            <p className="capitalize text-[#5A744B] text-lg md:text-xl font-normal">Price Per sq. Ft</p>
-          </div>
-          <div className="bg-[#C7D3A6] p-4 md:p-5 w-full">
-            <div className="flex items-end justify-start gap-1 capitalize">
-              <p className="text-[#31511E] font-bold text-3xl md:text-4xl">10,890.0</p>
-              <p className="text-[#31511E] font-normal text-xl md:text-2xl">Acres</p>
+            <div className="bg-[#D9E1C3] p-3 md:p-5 w-[80%] lg:w-full">
+              <p className="text-[#31511E] font-bold text-2xl md:text-4xl">1607</p>
+              <p className="capitalize text-[#5A744B] text-base md:text-xl font-normal">Price Per sq. Ft</p>
             </div>
-            <p className="capitalize text-[#5A744B] text-lg md:text-xl font-normal">Project Area</p>
-            <p className="capitalize text-[#5A744B] text-lg md:text-xl font-normal">A little detailed information about individual card in 2 lines</p>
+          </div>
+          <div className="bg-[#C7D3A6] p-3 md:p-5 w-full">
+            <div className="flex items-end justify-start gap-1 capitalize">
+              <p className="text-[#31511E] font-bold text-2xl md:text-4xl">10,890.0</p>
+              <p className="text-[#31511E] font-normal text-lg md:text-2xl">Acres</p>
+            </div>
+            <p className="capitalize text-[#5A744B] text-base md:text-xl font-normal">Project Area</p>
+            <p className="capitalize text-[#5A744B] text-base md:text-xl font-normal">A little detailed information about individual card in 2 lines</p>
           </div>
         </div>
       </div>
@@ -62,7 +63,7 @@ const ServicesHero = () => {
             <img src={assets.farmforest_2} alt="" className='w-full h-2/3' />
             <img src={assets.farmforest_3} alt="" className='w-full h-2/3' />
           </div>
-          <div className='flex items-center justify-start gap-4 md:gap-6'>
+          <div className='flex items-center justify-start gap-3 md:gap-6'>
             <Button className='flex items-center justify-center gap-1 md:gap-3 bg-[#859F3E] hover:bg-[#859F3E] w-40 h-10 md:w-60 md:h-14 text-white text-sm md:text-2xl font-bold leading-6 cursor-pointer'>
               Schedule Visit <Calendar className='size-4 md:size-6 pb-1' />
             </Button>
