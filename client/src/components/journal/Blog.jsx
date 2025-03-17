594eb9f9950834d447761d7113bc1fb9898b9fd1
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-17 17:21:21 +0530

    refactor Blog component; adjust text styles and spacing for improved readability

--- client/src/components/journal/Blog.jsx
+++ client/src/components/journal/Blog.jsx
@@ -16,21 +16,21 @@ const Blog = () => {
             <div className='grid grid-rows-[1fr_1fr_1fr] gap-11'>
               <div className='bg-[#F1F8DB] p-2 md:p-4 w-full flex items-center justify-between'>
                 <div className='size-36 md:size-42'><img src={assets.home_18} alt="" className='size-36 md:size-42' /></div>
-                <div className='group flex flex-col h-full items-start justify-end gap-2 md:gap-6 w-1/2 md:w-2/3'>
-                  <p className='capitalise text-[#31511E] font-normal text-xs sm:text-2xl leading-6 md:leading-8'>Whispers Of Bali - A serene Farmhouse Retreat In Hyderabad</p>
+                <div className='group flex flex-col h-full items-start justify-end gap-6 w-1/2 md:w-2/3'>
+                  <p className='capitalise text-[#31511E] font-normal text-xs sm:text-2xl leading-tight'>Whispers Of Bali - A serene Farmhouse Retreat In Hyderabad</p>
                   <Link to={'/journal/blog/1'} className='flex items-center gap-1 md:gap-4 text-[#859F3E] font-normal text-xs md:text-2xl leading-6 md:leading-8'>Read Now <ArrowUpRight className='size-3 md:size-8' /></Link>
                 </div>
               </div>
               <div className='bg-[#F1F8DB] p-2 md:p-4 w-full flex flex-row-reverse items-center justify-between'>
                 <div className='size-36 md:size-42'><img src={assets.home_19} alt="" className='size-36 md:size-42' /></div>
-                <div className='group flex flex-col h-full items-start justify-end gap-2 md:gap-6 w-1/2 md:w-2/3'>
-                  <p className='capitalise text-[#31511E] font-normal txet-xs md:text-2xl leading-6 md:leading-8'>5 farmlands near Mysuru on our site you can look at</p>
+                <div className='group flex flex-col h-full items-start justify-end gap-6 w-1/2 md:w-2/3'>
+                  <p className='capitalise text-[#31511E] font-normal text-xs md:text-2xl leading-tight'>5 farmlands near Mysuru on our site you can look at</p>
                   <Link to={'/journal/blog/1'} className='flex items-center gap-1 md:gap-4 text-[#859F3E] font-normal text-xs md:text-2xl leading-6 md:leading-8'>Read Now <ArrowUpRight className='size-3 md:size-8' /></Link>
                 </div>
               </div><div className='bg-[#F1F8DB] p-2 md:p-4 w-full flex items-center justify-between'>
                 <div className='size-36 md:size-42'><img src={assets.home_20} alt="" className='size-36 md:size-42' /></div>
-                <div className='group flex flex-col h-full items-start justify-end gap-2 md:gap-6 w-1/2 md:w-2/3'>
-                  <p className='capitalise text-[#31511E] font-normal txet-xs md:text-2xl leading-6 md:leading-8'>The vermicompost pioneet of Kashmir</p>
+                <div className='group flex flex-col h-full items-start justify-end gap-6 w-1/2 md:w-2/3'>
+                  <p className='capitalise text-[#31511E] font-normal text-xs md:text-2xl leading-tight'>The vermicompost pioneet of Kashmir</p>
                   <Link to={'/journal/blog/1'} className='flex items-center gap-1 md:gap-4 text-[#859F3E] font-normal text-xs md:text-2xl leading-6 md:leading-8'>Read Now <ArrowUpRight className='size-3 md:size-8' /></Link>
                 </div>
               </div>
