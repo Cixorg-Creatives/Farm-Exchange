7028d4f7cced0f3e0ba838806e3b39d543c6af3e
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-19 19:40:43 +0530

    enhance Featured and Recommended components with new layout and styling; add property details and images

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,3 +1,4 @@
+import { assets } from '@/assets/assets'
 import Build from '@/components/services/Build'
 import Call from '@/components/services/Call'
 import Difference from '@/components/services/Difference'
@@ -9,14 +10,32 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='px-4 md:px-6 lg:px-24'>
-      <ServiceHero />
-      <Grid />
-      <Build />
-      <Featured />
-      <Recommended />
-      <Call />
-      <Difference />
+    <div>
+      <div className='px-4 md:px-6 lg:px-24'>
+        <ServiceHero />
+        <Grid />
+        <Build />
+      </div>
+      <div className='relative'>
+        <div className='px-4 md:px-6 lg:px-24 relative'>
+          <Featured />
+        </div>
+        <div className='absolute top-0 right-0'>
+          <img src={assets.properties_2} alt="" className='w-24 md:w-48 lg:w-72 h-auto' />
+        </div>
+      </div>
+      <div className='relative'>
+        <div className='px-4 md:px-6 lg:px-24 relative'>
+          <Recommended />
+        </div>
+        <div className='absolute top-0 right-0'>
+          <img src={assets.properties_2} alt="" className='w-24 md:w-48 lg:w-72 h-auto' />
+        </div>
+      </div>
+      <div className='px-4 md:px-6 lg:px-24'>
+        <Call />
+        <Difference />
+      </div>
     </div>
   )
 }
