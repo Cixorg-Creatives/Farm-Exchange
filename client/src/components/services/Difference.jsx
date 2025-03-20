f6a8b186159220ba5f66a5a0994d99ed5af2b2f7
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-20 13:43:26 +0530

    enhance Difference component by replacing div with motion.div for improved animations and layout adjustments

--- client/src/components/services/Difference.jsx
+++ client/src/components/services/Difference.jsx
@@ -41,7 +41,7 @@ const Difference = () => {
       <div className='w-full pt-6 md:pt-10 xl:pt-14 flex flex-col gap-4 md:gap-8 lg:gap-12'>
         {
           data.map((item, index) => (
-            <div key={index} className='relative group hover:bg-[#D9E1C3] duration-150 ease-in px-1 md:px-3 lg:px-5 py-2 md:py-4 lg:py-6 flex justify-between shadow-inner shadow-black/25'>
+            <motion.div key={index} className='relative group hover:bg-[#D9E1C3] duration-150 ease-in px-1 md:px-3 lg:px-5 py-2 md:py-4 lg:py-6 flex justify-between shadow-inner shadow-black/25'>
               <div className='flex items-center gap-5 md:gap-7 lg:gap-10'>
                 <p className='text-[#B3B3B3] font-bold text-sm md:text-lg lg:text-2xl'>{item.index}</p>
                 <p className='capitalize text-[#02542D] font-normal text-base md:text-2xl lg:text-[2.5rem]'>{item.title}</p>
@@ -50,13 +50,13 @@ const Difference = () => {
                 <ArrowUpRight className='group-hover:block hidden duration-150 ease-in w-4 md:w-6 lg:w-10 h-auto' />
               </div>
               <motion.div 
-                className='lg:group-hover:block hidden absolute top-1/2 -translate-y-1/2 right-1/6 w-1/4 h-auto'
+                className='lg:group-hover:block hidden absolute inset-0'
                 variants={floatAnimation}
                 whileHover="hover"
               >
-                <img src={assets.services_4} alt="" className='w-full h-auto' />
+                <div className='w-full flex justify-end items-center'><img src={assets.services_4} alt="" className='w-1/4 h-auto -translate-y-1/6 -translate-x-3/5' /></div>
               </motion.div>
-            </div>
+            </motion.div>
           ))
         }
       </div>
