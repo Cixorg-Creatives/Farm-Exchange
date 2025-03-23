b9e6124ad77b05da12af7c1fe46b814e0b1c4e24
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-23 19:33:39 +0530

    add assets and configure project structure with Tailwind CSS and React Router

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { LogOut } from 'lucide-react';
+import { assets } from '@/assets/assets';
+
+const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        navigate('/');
+    }
+    return (
+        <div className='flex items-center justify-between mt-3 py-1 px-7 md:mt-5 md:py-2 md:px-28 w-full h-fit bg-[#073D2C] clashdisplay'>
+            <Link to={`/`} className='w-fit flex items-center justify-center gap-2 md:gap-3 lg:gap-4'>
+                <img src={assets.logo} alt="" className='h-[57.6px] w-[54px] sm:h-[76.8px] sm:w-[71.91px]' />
+                <h1 className='text-lg md:text-2xl lg:text-3xl text-[#747474]/60 font-semibold uppercase text-start leading-tight'>Admin <br /> panel</h1>
+            </Link>
+            <button onClick={handleLogout} className='bg-[#859F3E] border border-[#495823] px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs md:text-base lg:text-xl text-white font-normal flex gap-2 items-center justify-between'>Logout<LogOut className='h-3 md:h-4 lg:h-5 w-auto' /></button>
+        </div>
+    )
+}
+
+export default Navbar
