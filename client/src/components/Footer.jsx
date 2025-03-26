d014a209dd53c3f5c4dc95e269089e2fc860761e
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-26 13:23:00 +0530

    refactor Footer component to centralize navigation and contact details using data arrays for improved maintainability and consistency

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,42 +3,69 @@ import { Mail, MapPin, MapPinned, Phone } from 'lucide-react';
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+const data = [
+    {
+        name: 'Our Services',
+        link: '/services',
+    },
+    {
+        name: 'Farm Journal',
+        link: '/journal',
+    },
+    {
+        name: 'About Us',
+        link: '/about',
+    },
+    {
+        name: 'Contact Us',
+        link: '/contact',
+    },
+]
+
+const item = [
+    {
+        icon: <Phone className='size-4 md:size-5 lg:size-6 text-[#EFE6DD]' />,
+        detail: '+91 9848022338',
+    },
+    {
+        icon: <Mail className='size-4 md:size-5 lg:size-6 text-[#EFE6DD]' />,
+        detail: 'info@email.com',
+    },
+    {
+        icon: <MapPinned className='size-4 md:size-5 lg:size-6 text-[#EFE6DD]' />,
+        detail: 'Here comes the address of the company might be in two or three lines',
+    },
+]
+
 const Footer = () => {
     return (
         <div className='relative w-full footer_bg'>
-            <div className='bg-black/40 h-full w-full p-8 sm:p-12 md:p-16 lg:p-24 flex flex-col md:flex-row md:items-start lg:items-center md:justify-between gap-8 md:gap-12 lg:gap-0'>
+            <div className='bg-black/40 h-full w-full p-8 sm:p-12 md:p-16 lg:p-24 flex flex-col md:flex-row items-center justify-center md:justify-between gap-8 md:gap-12 lg:gap-0'>
                 <div className='z-10 flex flex-col items-center md:items-start w-full md:w-auto gap-6 lg:gap-12'>
                     <Link to="/" className='self-center md:self-start'>
                         <img src={assets.logo} alt="logo" className='h-32 w-auto md:h-48 lg:h-[203.34px] lg:w-[190.38px]' />
                     </Link>
                     <div className='flex flex-wrap justify-center md:justify-start gap-4 md:gap-6'>
-                        <NavLink to="/services" className="text-[#EFE6DD] text-sm md:text-base lg:text-xl">Our Services</NavLink>
-                        <NavLink to="/journal" className="text-[#EFE6DD] text-sm md:text-base lg:text-xl">Farm Journal</NavLink>
-                        <NavLink to="/about" className="text-[#EFE6DD] text-sm md:text-base lg:text-xl">About Us</NavLink>
-                        <NavLink to="/contact" className="text-[#EFE6DD] text-sm md:text-base lg:text-xl">Contact Us</NavLink>
+                        {
+                            data.map((item, index) => (
+                                <NavLink to={item.link} key={index} className="text-[#EFE6DD] text-xs md:text-base lg:text-xl">{item.name}</NavLink>
+                            ))
+                        }
                     </div>
                 </div>
-                <div className='z-10 flex flex-col items-center md:items-start w-full md:w-72 gap-6'>
-                    <h1 className='text-[#FFFFFF] text-lg md:text-xl font-medium'>Contact</h1>
-                    <div className='flex flex-col gap-4 md:gap-6'>
-                        <div className='flex items-center gap-4 md:gap-6'>
-                            <div className='bg-[#859F3E] flex items-center justify-center p-2 md:p-3 size-11'>
-                                <Phone size={24} className='text-[#EFE6DD]' />
-                            </div>
-                            <span className='text-[#859F3E] text-sm leading-4 md:text-base w-48 md:w-56 text-left'>+91 9848022338</span>
-                        </div>
-                        <div className='flex items-center gap-4 md:gap-6'>
-                            <div className='bg-[#859F3E] flex items-center justify-center p-2 md:p-3 size-11'>
-                                <Mail size={24} className='text-[#EFE6DD]' />
-                            </div>
-                            <span className='text-[#859F3E] text-sm leading-4 md:text-base w-48 md:w-56 text-left'>info@email.com</span>
-                        </div>
-                        <div className='flex items-center gap-4 md:gap-6'>
-                            <div className='bg-[#859F3E] flex items-center justify-center p-2 md:p-3 size-11'>
-                                <MapPinned size={24} className='text-[#EFE6DD]' />
-                            </div>
-                            <span className='text-[#859F3E] text-sm leading-4 md:text-base w-48 md:w-56 text-left'>Here comes the address of the company might be in two or three lines</span>
-                        </div>
+                <div className='z-10 flex flex-col items-center lg:items-start justify-center w-4/5 md:w-1/4 gap-6'>
+                    <h1 className='text-[#FFFFFF] text-base md:text-lg lg::text-xl font-medium'>Contact</h1>
+                    <div className='flex flex-col items-start justify-center gap-4 md:gap-6'>
+                        {
+                            item.map((item, index) => (
+                                <div key={index} className='w-full flex items-center justify-evenly lg:justify-around'>
+                                    <div className='bg-[#859F3E] flex items-center justify-center size-8 md:size-10 lg:size-12'>
+                                        {item.icon}
+                                    </div>
+                                    <span className='w-3/5 md:w-3/4 text-[#859F3E] text-xs md:text-sm lg:text-base leading-tight text-left'>{item.detail}</span>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div></div>
 
