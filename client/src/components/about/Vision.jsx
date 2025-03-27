aafa5ee5fbe3b462819076e3e354a93dd7c28dbc
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-27 18:32:48 +0530

    refactor Vision and Weekend components for improved layout and styling; enhance text clarity and responsiveness

--- client/src/components/about/Vision.jsx
+++ client/src/components/about/Vision.jsx
@@ -3,34 +3,34 @@ import React from 'react'
 
 const Vision = () => {
     return (
-        <div className='my-6 md:my-10 lg:my-14 flex flex-col xl:flex-row old-standard-tt xl:h-[95vh]'>
-            <div className='xl:w-36 xl:relative'>
-                <div className='xl:absolute xl:inset-0 xl:left-0 xl:bottom-0 z-10 w-[95vh] uppercase prata text-[#073D2C]/50 font-normal text-5xl sm:text-6xl md:text-7xl xl:text-8xl xl:-rotate-90'>our Vision</div>
-            </div>
-            <div className='my-6 md:my-10 lg:my-14 w-full xl:w-1/2 flex flex-col items-start'>
-                <div className='w-2/3 border-t-[2px] border-dashed border-[#859F3E]'></div>
-                <div className='flex'>
-                    <div className='xl:h-full border-r-[2px] border-dashed border-[#859F3E]'></div>
-                    <div className='flex flex-col items-start justify-start gap-4 sm:gap-6 md:gap-7 mt-2 sm:mt-4 xl:mt-7 ml-4 sm:ml-8 xl:ml-14'>
-                        <p className='w-full capitalize text-[#31511E] font-bold text-[2rem] sm:text-[2.5rem] md:text-[3rem] leading-[2.5rem] sm:leading-[3rem] md:leading-[3.5rem]'>farm exchange Pioneering Sustainable Agro-Realty</p>
-                        <p className='w-full capitalize text-black font-normal text-[1rem] sm:text-[1.25rem] xl:text-[1.5rem] leading-[1.5rem] sm:leading-[1.75rem] md:leading-[2rem] pr-0 sm:pr-6 md:pr-12 text-left'>We strive to be the most trusted agro-realty company, creating profitable and ethical farming businesses. Committed to organic farming, we prioritize soil  health, sustainability, and eco-friendly practices. By avoiding chemicals and embracing innovative  technologies, we ensure long-term fertility, environmental protection, and prosperity for our customers, employees, and vendors.</p>
+        <div className='py-6 md:py-10 lg:py-14 flex flex-col lg:flex-row items-center justify-between'>
+            <div className='w-full lg:w-3/5 flex flex-col lg:flex-row justify-between lg:relative'>
+                <div className='lg:w-2/5'>
+                    <div className='lg:absolute lg:inset-0 lg:-translate-x-1/4 z-10 lg:w-[85vh] uppercase text-[#073D2C]/50 font-semibold text-5xl md:text-7xl lg:text-[6.25rem] leading-tight lg:-rotate-90 text-left'>our Vision</div>
+                </div>
+                <div className='lg:w-[3/5] flex flex-col items-start'>
+                    <div className='w-2/3 border-t-[2px] border-dashed border-[#859F3E]'></div>
+                    <div className='flex'>
+                        <div className='lg:h-full border-r-[2px] border-dashed border-[#859F3E]'></div>
+                        <div className='flex flex-col items-start justify-start gap-4 sm:gap-6 md:gap-7 mt-3 sm:mt-5 lg:mt-7 ml-4 sm:ml-6 lg:ml-8'>
+                            <p className='w-full capitalize text-[#31511E] font-semibold text-xl md:text-3xl lg:text-6xl leading-tight'>farm exchange Pioneering Sustainable Agro-Realty</p>
+                            <p className='w-full capitalize text-black font-normal text-[1rem] sm:text-[1.25rem] lg:text-[1.5rem] leading-[1.5rem] sm:leading-[1.75rem] md:leading-[2rem] pr-0 sm:pr-6 md:pr-12 text-left'>We strive to be the most trusted agro-realty company, creating profitable and ethical farming businesses. Committed to organic farming, we prioritize soil  health, sustainability, and eco-friendly practices. By avoiding chemicals and embracing innovative  technologies, we ensure long-term fertility, environmental protection, and prosperity for our customers, employees, and vendors.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='w-full xl:w-[40%] h-[65vh] sm:h-[75vh] md:[85vh] flex flex-col items-end'>
-                <div className='flex'>
-                    <div className='h-2/3 border-r-[2px] border-dashed border-[#859F3E]'></div>
-                    <div className='relative flex flex-col items-end'>
-                        <img src={assets.about_6} alt="" className='w-40 sm:w-56 md:w-64 xl:w-72 h-60 sm:h-72 md:h-80 xl:h-96 ml-5 mb-5' />
-                        <div className='w-2/3 border-t-[2px] border-dashed border-[#859F3E]'></div>
-                        <div className='absolute top-2/3 -left-2/3'>
-                            <div className='flex flex-row-reverse items-end'>
-                                <div className='h-[24vh] sm:h-[27vh] md:h-[32vh] xl:h-[37vh] border-r-[2px] border-dashed border-[#859F3E]'></div>
-                                <div className='flex flex-col items-start'>
-                                    <div className='w-2/3 border-t-[2px] border-dashed border-[#859F3E]'></div>
-                                    <img src={assets.about_7} alt="" className='w-40 sm:w-56 md:w-64 xl:w-72 h-60 sm:h-72 md:h-80 xl:h-96 mr-5 mt-5' />
-                                </div>
-                            </div>
+            <div className='hidden lg:flex w-full lg:w-2/5 flex-col items-end'>
+                <div className='h-full w-full flex flex-col items-end'>
+                    <div className='flex w-full justify-end'>
+                        <div className='h-50 lg:h-[70%] border-r-[0.5px] md:border-r-1 lg:border-r-2 border-dashed border-[#859F3E]'></div>
+                        <img src={assets.about_6} alt="" className='w-3/5 h-auto ml-3 md:ml-4 lg:ml-5 mb-3 md:mb-4 lg:mb-5' />
+                    </div>
+                    <div className='w-[30%] border-t-[0.5px] md:border-t-1 lg:border-t-2 border-dashed border-[#859F3E]'></div>
+                    <div className='h-full flex flex-col item-start -translate-y-[30%] translate-x-[5%] '>
+                        <div className='w-[30%] border-t-[0.5px] md:border-t-1 lg:border-t-2 border-dashed border-[#859F3E]'></div>
+                        <div className='flex items-end'>
+                            <img src={assets.about_7} alt="" className='w-3/5 h-auto mr-3 md:mr-4 lg:mr-5 mt-3 md:mt-4 lg:mt-5' />
+                            <div className='h-50 lg:h-[70%] border-r-[0.5px] md:border-r-1 lg:border-r-2 border-dashed border-[#859F3E]'></div>
                         </div>
                     </div>
                 </div>
--- client/src/components/about/Weekend.jsx
+++ client/src/components/about/Weekend.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { assets } from '@/assets/assets';
-import { ArrowUpRight, Play } from 'lucide-react';
+import { Play } from 'lucide-react';
+import { Link } from 'react-router-dom'
+import Button from '../Button';
 
 const Weekend = () => {
     return (
-        <div className='flex flex-col lg:flex-row items-center justify-center gap-4 lg:gap-11 my-6 md:my-10 lg:my-14 lg:h-[49.375rem]'>
-            <div className='flex w-full lg:w-[36.4375rem]'>
-                <div className='w-full old-standard-tt flex flex-col items-start gap-4 md:gap-6'>
-                    <div className='w-full flex flex-col gap-3 md:gap-4'>
-                        <h2 className='text-[#859F3E] font-bold text-lg md:text-xl lg:text-[1.75rem] leading-8 md:leading-10 uppercase'>
-                            BUILD A COOL AND <br /> COMFORTABLE
+        <div className='flex flex-row items-center justify-between py-6 md:py-10 lg:py-14'>
+            <div className='flex w-1/2'>
+                <div className='w-full flex flex-col items-start gap-2 md:gap-4 md:gap-6'>
+                    <div className='w-full flex flex-col gap-1 md:gap-2 lg:gap-3'>
+                        <h2 className='boska font-normal text-[#859F3E] text-base md:text-xl lg:text-[1.75rem] leading-tight uppercase text-left'>
+                            BUILD A COOL AND  COMFORTABLE
                         </h2>
-                        <h1 className='text-[#31511E] font-bold text-3xl md:text-4xl lg:text-5xl leading-10 md:leading-[3.5rem] lg:leading-[4rem]'>
-                            Weekend home in a pristine 100 acres
+                        <h1 className='font-medium text-[#31511E] text-xl md:text-3xl lg:text-6xl leading-tight'>
+                            Weekend home <br /> in a pristine 100 <br /> acres
                         </h1>
                     </div>
-                    <div>
-                        <p className='text-[#31511E] font-normal text-base md:text-lg lg:text-2xl leading-6 md:leading-7 lg:leading-8 mb-4 md:mb-6'>
+                    <div className='flex flex-col gap-1 md:gap-2 lg:gap-3'>
+                        <p className='capitalize text-[#31511E] font-normal text-[8px] md:text-base lg:text-xl'>
                             A luxurious weekend home in a pristine landscape in the countryside. No more congested spaces. Own a sprawling space and build a lovely farm house on it. A pristine piece of land untouched by pollution, trespassing, city smoke, or any such nuisance. Thick greenery, landscaped areas, party amenities, club games, pools are a few highlights of this holiday home project. So as a resident you get to enjoy vacations without leaving your home.
                         </p>
-                        <p className='flex gap-2 md:gap-3 text-[#859F3E] font-normal text-base md:text-lg lg:text-2xl leading-6 md:leading-7 lg:leading-8 cursor-pointer'>
-                            View All <ArrowUpRight className='size-6 md:size-7' />
-                        </p>
+                        <Link to={'/journal/video'}><Button title={'View All'} icon={'show'} className='!text-[#859F3E] !p-0 text-[10px]' /></Link>
                     </div>
                 </div>
             </div>
-            <div className='relative w-full lg:w-[40.375rem]'>
-                <img src={assets.about_4} alt='' className='h-auto lg:h-[49.375rem] w-full lg:w-[40.375rem] object-cover' />
+            <div className='relative w-2/5'>
+                <img src={assets.about_4} alt='' className='h-auto w-full object-cover' />
                 <div className='z-10 border-[0.1875rem] md:border-[0.25rem] lg:border-[0.3125rem] border-white rounded-full size-16 md:size-20 lg:size-[6.2rem] bg-[#859F3E] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
                     <div className='flex items-center justify-center h-full w-full'><Play className='size-8 md:size-12 lg:size-[3.75rem] text-white fill-white' /></div>
                 </div>
