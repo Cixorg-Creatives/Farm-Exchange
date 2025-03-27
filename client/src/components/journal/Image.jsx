2a1197739fa7e23ee9aad44c561e8f752059a063
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-27 10:03:07 +0530

    refactor Image and JournalHero components for improved text clarity and responsiveness; enhance text styles and layout

--- client/src/components/journal/Image.jsx
+++ client/src/components/journal/Image.jsx
@@ -1,8 +1,7 @@
 import { assets } from '@/assets/assets'
 import React from 'react'
-import { Button } from '../ui/button'
-import { ArrowUpRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import Button from '../Button'
 
 const front = [assets.journal_15, assets.journal_16, assets.journal_17]
 const back = [assets.journal_19, assets.journal_22]
@@ -12,7 +11,7 @@ const Image = () => {
     <div className='py-6 md:py-10 lg:py-14 old-standard-tt flex flex-col gap-5 md:gap-10 lg:gap-20'>
       <div className='flex items-center justify-center'>
         <div className='relative w-4/5 md:w-3/4 lg:w-[64%] pl-2 md:pl-4 lg:pl-8'>
-          <p className='uppercase text-[#859F3E] font-bold text-lg md:text-7xl lg:text-[3.5rem]'>Capturing Nature's Essence, One Field at a Time.</p>
+          <p className='uppercase text-[#859F3E] font-medium text-2xl md:text-4xl lg:text-[3.5rem] leading-tight text-center'>Capturing Nature's Essence, One Field at a Time.</p>
           <img src={assets.open_double_quotes} alt="" className='absolute top-0 -translate-x-full -translate-y-1/3 left-0 size-5 md:size-10 lg:size-20' />
           <img src={assets.close_double_quotes} alt="" className='absolute bottom-0 translate-x-full translate-y-1/3 right-0 size-5 md:size-10 lg:size-20' />
         </div>
@@ -45,11 +44,9 @@ const Image = () => {
         </div>
       </div>
       <div className='px-4 md:px-6 lg:px-24 flex flex-col lg:flex-row items-start lg:items-center justify-between gap-2 md:gap-4 lg:gap-0'>
-        <div className='lg:w-[53%] capitalize text-[#747474] font-normal text-xs lg:text-xl'>Experience the beauty of nature through our farmland gallery. From lush green fields to golden harvests, every frame tells a story of hard work and dedication. Witness the harmony between the earth and those who nurture it. Explore the essence of farming, captured in every moment.</div>
+        <div className='capitalize text-[#747474] font-medium text-[10px] md:text-base lg:text-xl'>Experience the beauty of nature through our farmland gallery. From lush <br /> green fields to golden harvests, every frame tells a story of hard work <br /> and dedication. Witness the harmony between the earth and those who <br /> nurture it. Explore the essence of farming, captured in every moment.</div>
         <Link to={'/services'}>
-          <Button className='group rounded-none flex items-center justify-center gap-1 md:gap-2 lg:gap-3 bg-[#859F3E] w-36 h-10 md:w-56 md:h-16 lg:w-72 lg:h-20 old-standard-tt text-white text-sm md:text-lg lg:text-2xl font-bold leading-tight'>
-            Explore Services <ArrowUpRight className='group-hover:rotate-45 duration-150 ease-in size-4 md:size-6 lg:size-8' />
-          </Button>
+          <Button title='Explore Services' variant='primary' icon='show' />
         </Link>
       </div>
     </div>
--- client/src/components/journal/JournalHero.jsx
+++ client/src/components/journal/JournalHero.jsx
@@ -4,10 +4,10 @@ const data = [assets.journal_15, assets.journal_16, assets.journal_17, assets.jo
 
 const JournalHero = () => {
   return (
-    <div className='py-6 md:py-10 lg:py-14 old-standard-tt'>
+    <div className='py-6 md:py-10 lg:py-14'>
       <div className='bg-[#F6FCDF] w-full px-4 md:px-6 lg:px-32 flex flex-col items-center justify-center gap-2 md:gap-4 lg:gap-8'>
-        <p className='uppercase text-[#859F3E] font-bold text-sm md:text-lg lg:text-[1.75rem]'>farm journal</p>
-        <p className='uppercase text-[#073D2C] font-bold text-[19px] md:text-[2rem] lg:text-[4rem] text-center'>"Where Every Seed Tells a Story, And Every Field Holds a Memory."</p>
+        <p className='boska font-normal text-[#859F3E] text-base md:text-xl lg:text-[1.75rem] leading-tight uppercase text-center'>farm journal</p>
+        <p className='uppercase text-[#859F3E] font-semibold text-lg md:text-3xl lg:text-[3.5rem] text-center'>&apos;&apos;Where Every Seed Tells a Story, And Every Field Holds a Memory.&apos;&apos;</p>
       </div>
       <div className='relative flex flex-col items-center gap-2 md:gap-3 lg:gap-4 w-full overflow-hidden '>
         <img src={assets.ellipse1} alt="" className='w-full absolute top-0 -translate-y-1/2 scale-105' />
