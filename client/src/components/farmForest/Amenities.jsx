5cb2e4de50e4a13ea6ac0360b59c13c1a6e175e7
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-28 09:26:27 +0530

    refactor About, Amenities, and Location components for improved styling and layout; enhance text readability and responsiveness

--- client/src/components/farmForest/Amenities.jsx
+++ client/src/components/farmForest/Amenities.jsx
@@ -1,35 +1,54 @@
 import { assets } from '@/assets/assets'
 import React from 'react'
 
+const data=[
+  {
+    id:1,
+    title:'Club House',
+    image:assets.farmforest_4
+  },
+  {
+    id:2,
+    title:'Playing area',
+    image:assets.farmforest_5
+  },
+  {
+    id:3,
+    title:'Security',
+    image:assets.farmforest_6
+  },
+  {
+    id:4,
+    title:'Compounded Project',
+    image:assets.farmforest_7
+  },
+  {
+    id:5,
+    title:'Golf Pavilion',
+    image:assets.farmforest_8
+  },
+  {
+    id:6,
+    title:'Amphitheatre',
+    image:assets.farmforest_9
+  },
+]
+
 const Amenities = () => {
   return (
     <div className='py-4 md:py-6 lg:py-8 flex flex-col gap-2 md:gap-3 lg:gap-4'>
       <div className='uppercase sticky top-0 left-0 z-10 lg:relative text-[#31511E] font-bold bg-[#F6FCDF] text-[2rem]'>Amenities</div>
-      <div className='grid grid-cols-3 gap-7'>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_4} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>Club House</div>
-        </div>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_5} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>Playing area</div>
-        </div>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_6} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>security</div>
-        </div>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_7} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>Compounded Project</div>
-        </div>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_8} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>Golf Pavilion</div>
-        </div>
-        <div className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
-          <img src={assets.farmforest_9} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
-          <div className='capitalize p-1 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-bold text-[10px] sm:text-sm md:text-lg lg:text-2xl'>Amphitheatre</div>
-        </div>
+      <div className='grid grid-cols-[1fr_1fr_1fr] gap-3 md:gap-5 lg:gap-7'>
+        {
+          data.map((item)=>{
+            return(
+              <div key={item.id} className='col-span-1 row-span-1 flex flex-col items-start w-full h-full relative'>
+                <img src={item.image} alt="" className='w-full h-auto border-1 border-[#ADBF7E]' />
+                <div className='w-fit capitalize p-0.5 md:p-1.5 lg:p-2.5 bg-[#799138] -translate-y-1/2 flex items-center justify-center text-white font-semibold text-xs md:text-xl lg:text-2xl'>{item.title}</div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
--- client/src/components/farmForest/Location.jsx
+++ client/src/components/farmForest/Location.jsx
@@ -7,12 +7,12 @@ const Location = () => {
       <div className='uppercase sticky top-0 left-0 z-10 lg:relative text-[#31511E] font-bold bg-[#F6FCDF] text-[2rem]'>Location</div>
       <div className='flex flex-col gap-4 md:gap-8 lg:gap-11'>
         <img src={assets.farmforest_14} alt="" className='w-full h-auto border-1 border-black' />
-        <div className='w-2/3 lg:w-3/5 flex flex-col gap-2 md:gap-4 lg:gap-8'>
-          <h2 className="text-black capitalize font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl leading-tight text-left">
-            Discover Properties with the Best Value
+        <div className='flex flex-col gap-2 md:gap-4 lg:gap-8'>
+          <h2 className="text-[#31511E] capitalize font-medium text-2xl md:text-4xl lg:text-5xl leading-tight text-left">
+            Discover Properties with <br /> the Best Value
           </h2>
-          <p className="text-[#747474] capitalize font-normal text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed text-left">
-            This is a premium chance to gain access to <b>The Forest Farms</b> at the right destination. The property offers customizable cottages with serene landscape gardens.
+          <p className="text-[#31511E] capitalize font-normal text-xs md:text-base lg:text-xl leading-relaxed text-left">
+            This is a premium chance to gain access <br /> to <b>The Forest Farms</b> at the right <br /> destination. The property offers customizable <br /> cottages with serene landscape gardens.
           </p>
         </div>
       </div>
