970c4d7e7604c9bfa54f833b3c8d5b28d1cdec71
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-29 13:32:39 +0530

    refactor Blog component; simplify state management and remove unnecessary loading logic

--- admin/src/pages/Blog.jsx
+++ admin/src/pages/Blog.jsx
@@ -1,49 +1,15 @@
-import { assets } from '@/assets/assets';
-import BlogSettings from '@/components/blogs/BlogSettings';
-import axios from 'axios';
-import { createContext, useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Loader2 } from 'lucide-react';
-import BlogPreview from '@/components/blogs/BlogPreview';
-
-export const BlogContext = createContext({ });
+import { assets } from '@/assets/assets'
+import BlogPreview from '@/components/blogs/BlogPreview'
+import BlogSettings from '@/components/blogs/BlogSettings'
+import React from 'react'
 
 const Blog = () => {
-    const { blog_id } = useParams();
-    const navigate = useNavigate();
-    const [blog, setBlog] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetchBlog();
-    }, [blog_id]);
-
-    const fetchBlog = async () => {
-        try {
-            const { data } = await axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id });
-            setBlog(data.blog);
-            setLoading(false);
-        } catch (error) {
-            console.error(error);
-            setLoading(false);
-        }
-    };
-
-    const handleDelete = async () => {
-        try {
-            await axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/delete-blog", { blog_id });
-            navigate('/blogs');
-        } catch (error) {
-            console.error("Error deleting blog:", error);
-        }
-    };
-
     return (
         <div className='px-5 md:px-8 lg:px-12'>
-            <BlogSettings handleDelete={handleDelete} />
-            {loading ? <Loader2 /> : <BlogPreview blog={blog} />}
+            <BlogSettings />
+            <BlogPreview />
         </div>
-    );
-};
+    )
+}
 
-export default Blog;
+export default Blog
