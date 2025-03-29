6bf1b7a0d73c864b26eb3170cd10476976e51aeb
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-03-30 01:17:04 +0530

    remove commented-out grid layout in blog List component

--- client/src/components/blog/List.jsx
+++ client/src/components/blog/List.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const List = ({ blogs }) => {
     return (
         <div className='my-2 md:my-4 xl:my-6 clashdisplay'>
-            <div className='grid grid-cols-1 md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] gap-1.5 md:gap-x-3 lg:gap-x-5 gap-y-2 md:gap-y-4 lg:gap-y-6'>
+            {/* <div className='grid grid-cols-1 md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] gap-1.5 md:gap-x-3 lg:gap-x-5 gap-y-2 md:gap-y-4 lg:gap-y-6'>
                 {blogs.map((blog, index) => (
                     <Link to={`/journal/blog/${blog.blog_id}`} key={index} className={`grid-cols-1 px-2 md:px-4 lg:px-8 py-2.5 md:py-5 lg:py-9 border-1 border-[#D9E1C3] flex flex-col items-start gap-1 md:gap-2 lg:gap-4 cursor-pointer`}>
                         <img src={blog.banner} alt="" className='w-full h-auto aspect-[4/3] object-cover border-1 border-[#000000]' />
@@ -14,7 +14,7 @@ const List = ({ blogs }) => {
                         <p className='capitalize text-[#758A68] font-normal text-xs md:text-sm lg:text-base line-clamp-3'>{blog.des}</p>
                     </Link>
                 ))}
-            </div>
+            </div> */}
         </div>
     );
 };
