c790d64e9afc30ddf73b83a7507b6249b0f6d5db
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-04-01 01:42:18 +0530

    refactor EditProfile component; enhance layout and styling for better user experience

--- admin/src/pages/EditProfile.jsx
+++ admin/src/pages/EditProfile.jsx
@@ -121,42 +121,45 @@ const EditProfile = () => {
             <Toaster />
             {loading ? <Loader /> : (
                 <div>
-                    <h1 className="boska w-full uppercase text-black font-semibold text-lg lg:text-[2rem]">Edit Profile</h1>
-                    <div className='grid grid-cols-1 md:grid-cols-[1fr_4fr] items-start my-6'>
+                    <div className='w-full md:h-24 bg-[#D9E1C3] flex flex-col md:flex-row items-center justify-between boska py-3 md:py-0 px-2 md:px-4 lg:px-8 rounded-sm md:rounded-md lg:rounded-lg gap-3 md:gap-6 lg:gap-0'>
+                        <h1 className="boska w-full uppercase text-black font-semibold text-base md:text-lg lg:text-[2rem] text-start align-middle">
+                            Edit Profile
+                        </h1>
+                    </div>
+                    <div className='flex flex-col gap-2 md:gap-4 lg:gap-6 items-center my-6'>
                         <div className='flex flex-col items-center gap-3'>
-                            <label htmlFor="uploadImg" className="relative block w-48 h-48 bg-[#f3f3f3] rounded-full overflow-hidden">
+                            <label htmlFor="uploadImg" className="relative block w-2/5 h-auto aspect-square bg-[#f3f3f3] rounded-full overflow-hidden">
                                 <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center text-[#e0e0e0] font-bold text-lg opacity-0 hover:opacity-100 hover:backdrop-blur-md cursor-pointer">Upload Image</div>
                                 <img ref={profileImgEle} src={profile.personal_info.profile_img} alt="Profile" className='w-full h-auto rounded-full' />
                             </label>
                             <input id="uploadImg" type="file" accept=".jpeg, .png, .jpg" hidden onChange={handleImagePreview} />
                             <Button title='Upload' variant='ternary' onClick={handleImageUpload} />
                         </div>
-                        <div className='flex flex-wrap gap-4 px-5'>
+                        <div className="md:w-4/5 flex flex-col gap-1 md:gap-2 lg:gap-3">
                             <h1 className="w-full text-[#31511E] font-semibold text-lg">Admin Info</h1>
-                            <div className='w-full md:w-[24rem] bg-[#C7D3A6] p-4 rounded-md'>
-                                <div className='flex items-center gap-3'>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 lg:gap-6">
+                                <div className='col-span-1 flex items-center gap-3 bg-[#C7D3A6] p-4 rounded-md'>
                                     <User className="h-6 w-auto text-black" />
                                     <input className="w-full bg-transparent outline-none" placeholder="Full Name" value={profile.personal_info.fullname} disabled />
                                 </div>
-                            </div>
-                            <div className='w-full md:w-[24rem] bg-[#C7D3A6] p-4 rounded-md'>
-                                <div className='flex items-center gap-3'>
+                                <div className='col-span-1 flex items-center gap-3 bg-[#C7D3A6] p-4 rounded-md'>
                                     <Mail className="h-6 w-auto text-black" />
                                     <input className="w-full bg-transparent outline-none" placeholder="Email" value={profile.personal_info.email} disabled />
                                 </div>
                             </div>
+                        </div>
+                        <div className="md:w-4/5 flex flex-col gap-1 md:gap-2 lg:gap-3">
                             <h1 className="w-full text-[#31511E] font-semibold text-lg">Change Password</h1>
-
-                            <form ref={changePasswordForm} onSubmit={handleSubmitPassword} className="flex flex-col gap-4" >
-                                <div className="flex gap-4">
-                                    <div className='w-full md:w-[24rem] bg-[#C7D3A6] p-4 rounded-md flex items-center gap-3'>
+                            <form ref={changePasswordForm} onSubmit={handleSubmitPassword} className="w-full flex flex-col gap-4" >
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 lg:gap-6">
+                                    <div className='col-span-1 bg-[#C7D3A6] p-4 rounded-md flex items-center gap-3'>
                                         <LockKeyhole className="h-6 w-auto text-black" />
                                         <input type={showPassword ? "text" : "password"} name="currentPassword" required className="w-full bg-transparent outline-none" placeholder="Current Password" />
                                         <button type="button" onClick={() => setShowPassword(!showPassword)}>
                                             {showPassword ? <EyeOff className="h-6 w-auto text-black" /> : <Eye className="h-6 w-auto text-black" />}
                                         </button>
                                     </div>
-                                    <div className='w-full md:w-[24rem] bg-[#C7D3A6] p-4 rounded-md flex items-center gap-3'>
+                                    <div className='col-span-1 bg-[#C7D3A6] p-4 rounded-md flex items-center gap-3'>
                                         <LockKeyhole className="h-6 w-auto text-black" />
                                         <input type={showNewPassword ? "text" : "password"} name="newPassword" required className="w-full bg-transparent outline-none" placeholder="New Password" />
                                         <button type="button" onClick={() => setShowNewPassword(!showNewPassword)}>
@@ -166,7 +169,8 @@ const EditProfile = () => {
                                 </div>
                                 <Button type='submit' title='Submit' variant='ternary' className="w-fit" />
                             </form>
-                            <h1 className="w-full text-[#31511E] font-semibold text-lg">Add your social handles below</h1>
+                        </div>
+                        {/* <h1 className="w-full text-[#31511E] font-semibold text-lg">Add your social handles below</h1>
                             {[Youtube, Instagram, Facebook, Twitter].map((Icon, index) => (
                                 <div key={index} className='w-full md:w-[24rem] bg-[#C7D3A6] p-4 rounded-md'>
                                     <div className='flex items-center gap-3'>
@@ -175,8 +179,7 @@ const EditProfile = () => {
                                     </div>
                                 </div>
                             ))}
-                            <Button title='Update' variant='ternary' onClick={handleSubmit} />
-                        </div>
+                            <Button title='Update' variant='ternary' onClick={handleSubmit} /> */}
                     </div>
                 </div>
             )}
