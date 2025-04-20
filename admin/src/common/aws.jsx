560cb130dc4326762d85ae208ed9ac31266f78aa
DrXAyush <drxayush02@gmail.com>
2025-04-20 15:08:35 +0530

    aws

--- admin/src/common/aws.jsx
+++ admin/src/common/aws.jsx
@@ -6,9 +6,9 @@ const generateURL = async (image) => {
     const data = new FormData();
     data.append("file", image);
     data.append("upload_preset", "myCloud");
-    data.append("cloud_name", import.meta.env.CLOUDINARY_CLOUD_NAME);
+    data.append("cloud_name", "dmszykwlh");
 
-    const res = await fetch(`https://api.cloudinary.com/v1_1/${import.meta.env.CLOUDINARY_CLOUD_NAME}/image/upload`, {
+    const res = await fetch(`https://api.cloudinary.com/v1_1/dmszykwlh/image/upload`, {
         method: "POST",
         body: data
     })
